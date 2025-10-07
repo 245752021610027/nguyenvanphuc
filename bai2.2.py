46a878a8ef5031c4fcc2901f1c3826fb54417f9b
245752021610027 <nguyenvanphuc010106@icloud.com>
2025-10-07 16:35:22 +0700

    Add files via upload

---
+++ bai2.2.py
@@ -0,0 +1,13 @@
+print("nguyen van phuc")
+print("245752021610027")
+import math;
+x1=int(input("nhap x1--->"))
+y1=int(input("nhap y2--->"))
+
+x2=int(input("nhap x2--->"))
+y2=int(input("nhap y2--->"))
+
+d1 = (x2-x1) * (x2-x1);
+d2 =(y2-y1) * (y2-y1);
+kq = math.sqrt(d1+d2)
+print ("khoang cach giua hai diem:", kq);
---
+++ bai2.3.py
@@ -0,0 +1,7 @@
+print("nguyen van phuc")
+print("245752021610027")
+n=int(input('nhap n: '))
+if n%2==0:
+    print('%d la so chan' %n)
+else :
+    print('%d la so le' %n)
